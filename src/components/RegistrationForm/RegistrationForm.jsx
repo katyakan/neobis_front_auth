48515208ef7fd8cbfac5be0fe19ba2d48a1f6c29
katyakan <katyakan86@gmail.com>
2023-06-07 15:41:25 +0600

    feat: add tokens

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
 import s from './registrationForm.css';
 import smile from '../assets/img/smile.png';
 import { Formik, Field, Form } from 'formik';
+import { Navigate } from 'react-router-dom';
 
 const RegistrationForm = () => {
   const [dateValue, setDateValue] = useState('');
   const [isDateSelected, setIsDateSelected] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const { token } = useParams();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const response = await fetch(
+          `https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/check-token/${token}`
+        );
+        if (response.ok) {
+          setIsRegistered(true);
+        } else {
+          setIsRegistered(false);
+        }
+      } catch (error) {
+        setIsRegistered(false);
+      }
+    };
+
+    if (token) {
+      checkToken();
+    }
+  }, [token]);
 
   const handleDateChange = (e) => {
     const value = e.target.value;
@@ -19,6 +45,34 @@ const RegistrationForm = () => {
     setIsDateSelected(isFilled);
   };
 
+  const handleRegistration = async (values) => {
+    try {
+      const response = await fetch(
+        'https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/register-update/',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(values),
+        }
+      );
+
+      if (response.ok) {
+        alert('Registration successful!');
+        setIsRegistered(true);
+      } else {
+        alert('Registration failed. Please try again.');
+      }
+    } catch (error) {
+      alert('Registration failed. Please try again.');
+    }
+  };
+
+  if (isRegistered) {
+    return <Navigate to="/passwordset" />;
+  }
+
   return (
     <div className="form">
       <img src={smile} alt="smile" />
@@ -30,29 +84,7 @@ const RegistrationForm = () => {
           email: '',
           date: '',
         }}
-        onSubmit={async (values) => {
-          try {
-            const response = await fetch(
-              'http://34.107.1.158/auth/register-update/',
-              {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
-              }
-            );
-
-            if (response.ok) {
-              alert('Registration successful!');
-              // сюда редирект на след страницу, если код успешный
-            } else {
-              alert('Registration failed. Please try again.');
-            }
-          } catch (error) {
-            alert('Registration failed. Please try again.');
-          }
-        }}
+        onSubmit={handleRegistration}
         onChange={handleFormChange}
       >
         <Form className="registration-form">
@@ -64,8 +96,7 @@ const RegistrationForm = () => {
             <Field id="lastName" name="lastName" placeholder=" " />
             <label htmlFor="lastName">Фамилия</label>
           </div>
-
-          <div className={`input-container ${isDateSelected ? 'filled' : ''}`}>
+          <div className="input-container">
             <Field
               id="date"
               name="date"
