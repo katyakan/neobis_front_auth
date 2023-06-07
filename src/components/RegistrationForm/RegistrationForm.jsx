46c5c7e097ed27b7bb2f08e6eb9c45048ef0188e
katyakan <katyakan86@gmail.com>
2023-06-07 23:08:32 +0600

    feat: add headers

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import s from './registrationForm.css';
 import smile from '../assets/img/smile.png';
 import { Formik, Field, Form } from 'formik';
 import { Navigate } from 'react-router-dom';
@@ -15,7 +16,12 @@ const RegistrationForm = () => {
     const checkToken = async () => {
       try {
         const response = await fetch(
-          `https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/register-update/?token=${token}`
+          `https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/register-update/`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
         if (response.ok) {
           setIsRegistered(true);
@@ -39,19 +45,20 @@ const RegistrationForm = () => {
   };
 
   const handleFormChange = (values) => {
-    const { firstName, lastName, email } = values;
-    const isFilled = firstName && lastName && email && dateValue;
+    const { firstName, lastName, phone, date } = values;
+    const isFilled = firstName && lastName && phone && date;
     setIsDateSelected(isFilled);
   };
 
   const handleRegistration = async (values) => {
     try {
       const response = await fetch(
-        'https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/register-update/',
+        `https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/register-update/`,
         {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify(values),
         }
@@ -80,42 +87,64 @@ const RegistrationForm = () => {
         initialValues={{
           firstName: '',
           lastName: '',
-          email: '',
+          phone: '',
           date: '',
         }}
         onSubmit={handleRegistration}
-        onChange={handleFormChange}
       >
-        <Form className="registration-form">
-          <div className="input-container">
-            <Field id="firstName" name="firstName" placeholder=" " />
-            <label htmlFor="firstName">Имя</label>
-          </div>
-          <div className="input-container">
-            <Field id="lastName" name="lastName" placeholder=" " />
-            <label htmlFor="lastName">Фамилия</label>
-          </div>
-          <div className="input-container">
-            <Field
-              id="date"
-              name="date"
-              placeholder=" "
-              type="date"
-              value={dateValue}
-              onChange={handleDateChange}
-            />
-            <label htmlFor="date">Дата рождения</label>
-          </div>
-          <button
-            type="submit"
-            className={`submit-button ${isDateSelected ? 'filled' : ''}`}
-          >
-            Регистрация
-          </button>
-        </Form>
+        {({ values, handleChange }) => (
+          <Form className="registration-form">
+            <div className="input-container">
+              <Field
+                id="firstName"
+                name="firstName"
+                placeholder=" "
+                onChange={handleChange}
+              />
+              <label htmlFor="firstName">Имя</label>
+            </div>
+            <div className="input-container">
+              <Field
+                id="lastName"
+                name="lastName"
+                placeholder=" "
+                onChange={handleChange}
+              />
+              <label htmlFor="lastName">Фамилия</label>
+            </div>
+            <div className="input-container">
+              <Field
+                id="phone"
+                name="phone"
+                placeholder=" "
+                onChange={handleChange}
+              />
+              <label htmlFor="phone">Номер телефона</label>
+            </div>
+            <div className="input-container">
+              <Field
+                id="date"
+                name="date"
+                placeholder=" "
+                type="date"
+                value={values.date}
+                onChange={handleChange}
+              />
+              <label htmlFor="date">Дата рождения</label>
+            </div>
+
+            <button
+              type="submit"
+              className={`submit-button ${isDateSelected ? 'filled' : ''}`}
+            >
+              Регистрация
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
 };
 
 export default RegistrationForm;
+               
