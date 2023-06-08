ed3becb3c33e904a6462e91a2530e654907bbe7e
katyakan <katyakan86@gmail.com>
2023-06-09 00:15:04 +0600

    feat: remove tokens

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -15,8 +15,6 @@ const RegistrationForm = () => {
   useEffect(() => {
     const checkToken = async () => {
       try {
-        //const urlParams = new URLSearchParams(window.location.search);
-        //const tokenParam = urlParams.get('token');
         console.log(token);
         const response = await fetch(
           `https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/register-update/`,
