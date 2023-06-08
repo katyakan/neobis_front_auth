92c376832a29be32d41fceeb3cb1a6b2fe27d221
katyakan <katyakan86@gmail.com>
2023-06-08 14:16:12 +0600

    feat: add api to passwordReset page

--- src/components/PasswordReset/PasswordReset.jsx
+++ src/components/PasswordReset/PasswordReset.jsx
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import smile from '../assets/img/smile.png';
-import axios from 'axios';
-import styles from '../Registration/registration.css';
-import s from './passwordReset.css';
+import styles from './passwordReset.css';
 
-import {
-  setTokens,
-  setLoading,
-} from '../../redux/actions/passwordResetActions';
-
-const PasswordReset = () => {
+const Registration = () => {
   const [isFormFilled, setIsFormFilled] = useState(false);
-  const dispatch = useDispatch();
+  const [isRequestSuccessful, setIsRequestSuccessful] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -21,21 +13,27 @@ const PasswordReset = () => {
     },
     onSubmit: async (values) => {
       try {
-        const response = await axios.post(
-          'http://34.107.1.158/auth/password_reset/',
-          values,
+        const response = await fetch(
+          'https://cors-anywhere.herokuapp.com/http://34.107.1.158/auth/password_reset/',
           {
+            method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-              'X-CSRFToken':
-                '7fWplB0tjkVeq3giGoHzoUpnkURbLQl4ap09h4N3m4DFDNKszHx2VbsYLQwb5fUJ',
             },
+            body: JSON.stringify({ email: values.email }),
           }
         );
-        const data = response.data;
-        console.log('Response:', data);
+
+        if (response.ok) {
+          setIsRequestSuccessful(true);
+          const data = await response.json();
+          console.log('Successful response:', data);
+        } else {
+          const errorData = await response.json();
+          console.log('Error response:', errorData);
+        }
       } catch (error) {
-        console.error('Error resetting password:', error);
+        console.log('Fetch error:', error);
       }
     },
   });
@@ -48,32 +46,36 @@ const PasswordReset = () => {
     <div className="form">
       <img src={smile} alt="Smile" />
       <h2>Сброс пароля</h2>
-      <p className="description">
-        На введенную вами почту мы отправим ссылку, перейдя по которой вы
-        сможете сбросить пароль
-      </p>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="input-container">
-          <input
-            id="email-register"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            placeholder=" "
-          />
-          <label htmlFor="email-register">Электронная почта</label>
-        </div>
+      {isRequestSuccessful ? (
+        <p>Мы отправили вам письмо на почту.</p>
+      ) : (
+        <form onSubmit={formik.handleSubmit}>
+          <div className="input-container">
+            <p className="description">
+              На введенную вами почту мы отправим ссылку, перейдя по которой вы
+              сможете сбросить пароль
+            </p>
+            <input
+              id="email-register"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              placeholder=" "
+            />
+            <label htmlFor="email-register">Электронная почта</label>
+          </div>
 
-        <button
-          type="submit"
-          className={`submit-button ${isFormFilled ? 'filled' : ''}`}
-        >
-          Далее
-        </button>
-      </form>
+          <button
+            type="submit"
+            className={`submit-button ${isFormFilled ? 'filled' : ''}`}
+          >
+            Далее
+          </button>
+        </form>
+      )}
     </div>
   );
 };
 
-export default PasswordReset;
+export default Registration;
