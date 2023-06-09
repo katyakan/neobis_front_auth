d942c74ddee160966ce2a6f04267e09db6dab2ef
katyakan <katyakan86@gmail.com>
2023-06-10 01:43:44 +0600

    fix: add import useParams

--- src/components/mobile/Redirection.jsx
+++ src/components/mobile/Redirection.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const Redirection = () => {
   const navigate = useNavigate();
